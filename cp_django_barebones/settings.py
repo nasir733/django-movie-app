3f89d5ab4b0f3c76dd114b30a59598abc9563f08
engnasir733 <engnasir733@gmail.com>
2020-03-30 09:46:26 +0000

    did sharma changes

--- cp_django_barebones/settings.py
+++ cp_django_barebones/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cp_django_barebones.urls'
